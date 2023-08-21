8207e9e3cc1bf8181dfe911ff483c167e2807fe9
sardarsj <sardarsjsingh08@gmail.com>
2023-08-21 12:55:54 +0530

    Frontend to backend data pass

--- backend/server.js
+++ backend/server.js
@@ -2,28 +2,38 @@ const express = require("express");
 const dotenv = require("dotenv"); //port vgera lyi use kita h
 const { chats } = require("./data/data"); //data of chats coming from data.js
 const connectDB = require("./config/db");
+const userRoutes = require("./routes/userRoutes");
+const { errorHandler, notFound } = require("./middleware/errorMiddleware");
 
 
 dotenv.config();
 connectDB();
 const app = express();
 
+app.use(express.json());
+
 //making get request to / route
 app.get("/", (req, res) => {
   res.send("API is running successfully"); 
 });
 
-///api/chats is an endpoint
-app.get("/api/chat", (req, res) => {
-  res.send(chats); //res shyd display krda h data on screen (have to check on internet)
-});
+app.use('/api/user', userRoutes);
 
-app.get("/api/chat/:id", (req, res) => {
-  // console.log(req.params.id);
-  const singleChat = chats.find((c) => c._id === req.params.id);
-  res.send(singleChat);
-});
+app.use(notFound);
+app.use(errorHandler);
+
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, console.log(`Server started on PORT ${PORT}`));
+
+// ///api/chats is an endpoint
+// app.get("/api/chat", (req, res) => {
+//   res.send(chats); //res shyd display krda h data on screen (have to check on internet)
+// });
+
+// app.get("/api/chat/:id", (req, res) => {
+//   // console.log(req.params.id);
+//   const singleChat = chats.find((c) => c._id === req.params.id);
+//   res.send(singleChat);
+// });
