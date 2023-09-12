296f14994e35e540ae50a507fa583b4b7e3f7209
sardarsj <sardarsjsingh08@gmail.com>
2023-09-12 17:45:47 +0530

    some updates

--- backend/server.js
+++ backend/server.js
@@ -1,44 +1,65 @@
 const express = require("express");
-const dotenv = require("dotenv"); //port vgera lyi use kita h
-const { chats } = require("./data/data"); //data of chats coming from data.js
 const connectDB = require("./config/db");
+const dotenv = require("dotenv");
 const userRoutes = require("./routes/userRoutes");
 const chatRoutes = require("./routes/chatRoutes");
 const messageRoutes = require("./routes/messageRoutes");
-const { errorHandler, notFound } = require("./middleware/errorMiddleware");
-
+const { notFound, errorHandler } = require("./middleware/errorMiddleware");
+const path = require("path");
+// const server = require('server')
 dotenv.config();
 connectDB();
 const app = express();
 
-app.use(express.json());
+app.use(express.json()); // to accept json data
 
-//making get request to / route
-app.get("/", (req, res) => {
-  res.send("API is running successfully");
-});
+// app.get("/", (req, res) => {
+//   res.send("API Running!");
+// });
 
 app.use("/api/user", userRoutes);
 app.use("/api/chat", chatRoutes);
 app.use("/api/message", messageRoutes);
 
+// --------------------------deployment------------------------------
+
+// const __dirname1 = path.resolve();
+
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname1, "/frontend/build")));
+
+//   app.get("*", (req, res) =>
+//     res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))
+//   );
+// } else {
+//   app.get("/", (req, res) => {
+//     res.send("API is running..");
+//   });
+// }
+
+// --------------------------deployment------------------------------
+
+// Error Handling middlewares
 app.use(notFound);
 app.use(errorHandler);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 
-const server = app.listen(PORT, console.log(`Server started on PORT ${PORT}`));
+const server = app.listen(
+  PORT,
+  console.log(`Server running on PORT ${PORT}...`)
+);
 
 const io = require("socket.io")(server, {
   pingTimeout: 60000,
   cors: {
     origin: "http://localhost:3000",
+    // credentials: true,
   },
 });
 
 io.on("connection", (socket) => {
-  console.log("connected to socket.io");
-
+  console.log("Connected to socket.io");
   socket.on("setup", (userData) => {
     socket.join(userData._id);
     socket.emit("connected");
@@ -46,9 +67,8 @@ io.on("connection", (socket) => {
 
   socket.on("join chat", (room) => {
     socket.join(room);
-    socket.emit("User joined room" + room);
+    console.log("User Joined Room: " + room);
   });
-
   socket.on("typing", (room) => socket.in(room).emit("typing"));
   socket.on("stop typing", (room) => socket.in(room).emit("stop typing"));
 
@@ -60,7 +80,7 @@ io.on("connection", (socket) => {
     chat.users.forEach((user) => {
       if (user._id == newMessageRecieved.sender._id) return;
 
-      socket.in(user._id).emit("message received", newMessageRecieved);
+      socket.in(user._id).emit("message recieved", newMessageRecieved);
     });
   });
 
@@ -69,6 +89,105 @@ io.on("connection", (socket) => {
     socket.leave(userData._id);
   });
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const express = require("express");
+// const dotenv = require("dotenv"); //port vgera lyi use kita h
+// const { chats } = require("./data/data"); //data of chats coming from data.js
+// const connectDB = require("./config/db");
+// const userRoutes = require("./routes/userRoutes");
+// const chatRoutes = require("./routes/chatRoutes");
+// const messageRoutes = require("./routes/messageRoutes");
+// const { errorHandler, notFound } = require("./middleware/errorMiddleware");
+
+// dotenv.config();
+// connectDB();
+// const app = express();
+
+// app.use(express.json());
+
+// //making get request to / route
+// app.get("/", (req, res) => {
+//   res.send("API is running successfully");
+// });
+// //gpt code
+
+// const cors = require('cors');
+// // const app = express();
+
+// app.use(cors({
+//   origin: 'http://127.0.0.1:5000/',
+//   methods: ['GET', 'POST', 'PUT'],
+//   credentials: true, // if you're using cookies or other credentials
+// }));
+
+
+
+
+// app.use("/api/user", userRoutes);
+// app.use("/api/chat", chatRoutes);
+// app.use("/api/message", messageRoutes);
+
+// app.use(notFound);
+// app.use(errorHandler);
+
+// const PORT = process.env.PORT || 5000;
+
+// const server = app.listen(PORT, console.log(`Server started on PORT ${PORT}`));
+
+// const io = require("socket.io")(server, {
+//   pingTimeout: 60000,
+//   cors: {
+//     origin: "http://localhost:3000",
+//   },
+// });
+
+// io.on("connection", (socket) => {
+//   console.log("connected to socket.io");
+
+//   socket.on("setup", (userData) => {
+//     socket.join(userData._id);
+//     socket.emit("connected");
+//   });
+
+//   socket.on("join chat", (room) => {
+//     socket.join(room);
+//     socket.emit("User joined room" + room);
+//   });
+
+//   socket.on("typing", (room) => socket.in(room).emit("typing"));
+//   socket.on("stop typing", (room) => socket.in(room).emit("stop typing"));
+
+//   socket.on("new message", (newMessageRecieved) => {
+//     var chat = newMessageRecieved.chat;
+
+//     if (!chat.users) return console.log("chat users not defined");
+
+//     chat.users.forEach((user) => {
+//       if (user._id == newMessageRecieved.sender._id) return;
+
+//       socket.in(user._id).emit("message received", newMessageRecieved);
+//     });
+//   });
+
+//   socket.off("setup", () => {
+//     console.log("USER DISCONNECTED");
+//     socket.leave(userData._id);
+//   });
+// });
 
 // ///api/chats is an endpoint
 // app.get("/api/chat", (req, res) => {
